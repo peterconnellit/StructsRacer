8f2c6f618fe9ab5ad772043e0c199d639f6fa0fb
Peter Connell <peterconnellit@gmail.com>
2022-10-15 16:07:25 +0100

    sort

--- StructsRacer.cpp
+++ StructsRacer.cpp
@@ -30,9 +30,10 @@ int main()
 	//max hours of race
 	const int MAX_HOURS = 5;
 
-	srand((0));
+	//srand((0));
+	srand(time(NULL));
 
-	//for loop runs 5 times, crearing rand number for each racers distance to store in distance variable, then add to next iteration
+	//for loop runs 5 times, creating rand number for each racers distance to store in distance variable, then add to next iteration
 	for (int i = 0; i < MAX_HOURS; i++) {
 		team1.distance = (rand() % 10) + team1.distance;
 		cout << team1.teamName << "'s distance is: " << team1.distance << endl;
@@ -49,7 +50,7 @@ int main()
 	int n = sizeof(totalDistances) / sizeof(totalDistances[0]);
 
 	//sort array
-	sort(totalDistances,totalDistances + n);
+	sort(totalDistances, totalDistances + n);
 
 	//winner and loser from sorted array
 	int winner = totalDistances[2];
@@ -59,4 +60,5 @@ int main()
 	cout << "Least distance travelled: " << loser << endl;
 
 	//still needs output to say the winner is...red or blue or yellow
+	//cout << winner << teamName << endl;
 }
